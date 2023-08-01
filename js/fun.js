3f03087693f1ddfdce12ba6af0cdfb011d48a7c4
Abdul Hannan <119074296+abdulhannanshaikhh@users.noreply.github.com>
2023-08-01 14:02:23 +0530

    Add files via upload

---
+++ js/fun.js
@@ -0,0 +1,12 @@
+function myFunction3() {
+  document.getElementById("demo1").innerHTML = "Our rental process is designed to be simple and hassle-free, and our team is dedicated to providing exceptional Our rental process is designed to be simple and hassle-free, and our team is dedicated to providing exceptional customer service from start to finish. Whether you need a car for a special occasion, business travel, or just to enjoy the thrill of driving a luxury vehicle, we are here to help. With our luxury car rental service, you can enjoy the freedom of the open road while experiencing the power and elegance of a high-performance luxury car.";
+}
+function toggleAnswer(id) {
+      var answer = document.getElementById(id);
+      if (answer.style.display === 'none') {
+        answer.style.display = 'block';
+      } else {
+        answer.style.display = 'none';
+      }
+    }
+      
